d1858d96a3b0ebec3d9962fbc25937441f5144c1
pikpoket <pik-poket@live.fr>
2023-04-01 11:21:38 +0200

    action triggers

--- PlayerRobot.gd
+++ PlayerRobot.gd
@@ -5,23 +5,30 @@ var velocity
 var player = "p1"
 var state = "stop"
 
+var robotDisabled = false
+
 func ready():
 	if player == "p2":
 		$AnimatedSprite.z_index = -1
 		$AnimatedSprite.offset = Vector2(0, -50)
 
 func get_input():
-	var input_direction
+	var input_direction = Vector2(0,0)
 	if player == "p1":
-		input_direction = Input.get_vector("ui_accept", "p1_right", "ui_accept", "ui_accept")
+		if not robotDisabled:
+			work()
+			input_direction = Input.get_vector("ui_accept", "p1_right", "ui_accept", "ui_accept")
 	elif player == "p2":
-		input_direction = Input.get_vector("ui_accept", "p2_right", "ui_accept", "ui_accept")
+		if not robotDisabled:
+			work()
+			input_direction = Input.get_vector("ui_accept", "p2_right", "ui_accept", "ui_accept")
 		
 	velocity = input_direction * speed
 
 func _physics_process(delta):
 	get_input()
 	check_state()
+	
 	move_and_slide(velocity)
 	position.y -= delta * speed * -1
 
@@ -36,6 +43,8 @@ func check_state():
 func broke():
 	if state == "work":
 		state = "broke"
+		robotDisabled = true
+		$disabledTimer.start()
 
 func work():
 	if state == "stop":
@@ -50,7 +59,16 @@ func _input(event):
 		if player == "p1":
 			if Input.is_action_pressed("p1_action"):
 				print("p1 acted")
+			if Input.is_action_pressed("p1_stop"):
+				print("p1 stopped")
+				stop()
 		elif player == "p2":
 			if Input.is_action_pressed("p2_action"):
 				print("p2 acted")
+			if Input.is_action_pressed("p2_stop"):
+				print("p2 stopped")
+				stop()
 
+
+func _on_disabledTimer_timeout():
+	robotDisabled = false
