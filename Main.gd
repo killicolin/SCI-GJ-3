1c001234b4db03e2ebad6eff0a8ab837d9043cb6
pikpoket <pik-poket@live.fr>
2023-04-02 11:49:25 +0200

    commented QTE

--- Main.gd
+++ Main.gd
@@ -11,11 +11,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Play_pressed():
 	get_tree().change_scene("res://Level1.tscn")
 
