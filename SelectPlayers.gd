5406b210a620caf2adab8821fd11c50b00f6789f
pikpoket <pik-poket@live.fr>
2023-04-02 15:30:17 +0200

    full sound ui select player

--- SelectPlayers.gd
+++ SelectPlayers.gd
@@ -14,14 +14,49 @@ func _ready():
 
 func _on_2_pressed():
 	Singleton.playerNb = 2
+	$bip.play()
 	get_tree().change_scene("res://Level2.tscn")
 
 
 func _on_3_pressed():
 	Singleton.playerNb = 3
+	$bip.play()
+	
 	get_tree().change_scene("res://Level2.tscn")
 
 
 func _on_4_pressed():
 	Singleton.playerNb = 4
+	$bip.play()
+	
 	get_tree().change_scene("res://Level2.tscn")
+
+func _on_2_mouse_entered():
+	$HBoxContainer/VBoxContainer.get_node("2").modulate = Color(0.5,0.5,0.5)
+
+func _on_2_mouse_exited():
+	$HBoxContainer/VBoxContainer.get_node("2").modulate = Color(1,1,1)
+	
+
+func _on_3_mouse_entered():
+	$HBoxContainer/VBoxContainer.get_node("3").modulate = Color(0.5,0.5,0.5)
+	
+
+func _on_3_mouse_exited():
+	$HBoxContainer/VBoxContainer.get_node("3").modulate = Color(1,1,1)
+	
+
+func _on_4_mouse_entered():
+	$HBoxContainer/VBoxContainer.get_node("4").modulate = Color(0.5,0.5,0.5)
+	
+
+func _on_4_mouse_exited():
+	$HBoxContainer/VBoxContainer.get_node("4").modulate = Color(1,1,1)
+	
+
+#func _on_Play_mouse_entered():
+#	$VBoxContainer/Play.modulate = Color(0.5,0.5,0.5)
+#
+#
+#func _on_Play_mouse_exited():
+#	$VBoxContainer/Play.modulate = Color(1,1,1)
