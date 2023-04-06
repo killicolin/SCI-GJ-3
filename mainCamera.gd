371be26f5dde1ee5fe9fd0f560dabcfc5f2b7178
spacha <thisissparta@gmail.com>
2023-04-06 17:19:29 +0200

    delete old test sprite, working parallax, smooth zoom changes

--- mainCamera.gd
+++ mainCamera.gd
@@ -1,5 +1,5 @@
 extends Camera2D
-
+#
 export var dezoom_max = 2
 export var dist_max = 800
 onready var player_nb = 0
@@ -14,8 +14,20 @@ class Sorter:
 func _ready():
 	# Initialization here
 	set_process(true)
+
+func reposBackground():
+	var ref1 = $"../ParallaxBackground/ParallaxLayer/Sprite"
+	var ref2 = $"../ParallaxBackground/ParallaxLayer2/Sprite"
+	var ref3 = $"../ParallaxBackground/ParallaxLayer3/Sprite"
 	
+	ref1.offset.y = self.position.y
+	ref2.offset.y = self.position.y
+	ref3.offset.y = self.position.y
+	
+	pass
+
 func _process(delta):
+	reposBackground()
 	
 	var player_still_screen = []
 	var sorter = Sorter.new()
@@ -33,7 +45,13 @@ func _process(delta):
 			tmp_new_pos=newpos/i
 			player_still_screen.append(nb)
 	newpos = newpos/ i
-	self.global_position = newpos
+	
+	var tween = get_node("Tween")
+	tween.interpolate_property(self, "global_position",
+			self.global_position, newpos, 1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	
 	var distance = 0
 	for nb1 in player_still_screen:
 		for nb2 in player_still_screen:
@@ -41,14 +59,19 @@ func _process(delta):
 
 	var zoom_factor = distance * 0.002
 	var min_zoom = 1
+	
+	var newZoom = 1
+	
 	if i == player_nb:
-		print("YOLO")
-		print(max(zoom_factor / 2, min_zoom))
-		set_zoom(Vector2(1,1) * max(zoom_factor / 2, min_zoom))
+		newZoom = Vector2(1,1) * max(zoom_factor / 2, min_zoom)
 	else :
-		print("nOO")
-		print(i)
-		set_zoom(Vector2(1,1) *dezoom_max)
-	if self.zoom.x != 1.0 :
+		newZoom = Vector2(1,1) *dezoom_max
 
+	var tween2 = get_node("Tween")
+	tween2.interpolate_property(self, "zoom",
+			zoom, newZoom, 1,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween2.start()
+	
+	if self.zoom.x != 1.0 :
 		emit_signal("zoom_change")
